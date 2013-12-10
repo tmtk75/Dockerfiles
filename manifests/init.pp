89696a5ac63db34a48debf225bfc56f46c01b5b0
Tomotaka Sakuma <ktmtmks@gmail.com>
2013-12-10 22:29:47 +0900

    update versions for vbox and vagrant

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,18 +5,18 @@ class virtualbox::darwin {
     refreshonly => true,
   }
 
-  package { 'VirtualBox-4.3.2-90405-OSX.dmg':
+  package { 'VirtualBox-4.3.4-91027-OSX.dmg':
     ensure   => installed,
     provider => 'pkgdmg',
-    source   => 'http://download.virtualbox.org/virtualbox/4.3.2/VirtualBox-4.3.2-90405-OSX.dmg',
+    source   => 'http://download.virtualbox.org/virtualbox/4.3.4/VirtualBox-4.3.4-91027-OSX.dmg',
     require  => Exec['Kill Virtual Box Processes'],
   }
 }
 
 class vagrant::darwin {
-  package { 'Vagrant.1.3.5.dmg':
+  package { 'Vagrant-1.4.0.dmg':
     ensure   => installed,
-    source   => 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant-1.3.5.dmg',
+    source   => 'https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.0.dmg',
     provider => 'pkgdmg'
   }
 }

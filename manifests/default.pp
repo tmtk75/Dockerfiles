c8f7579c928a043a94b2d9679820561958f2cd9c
Tomotaka Sakuma <ktmtmks@gmail.com>
2013-12-11 08:42:34 +0900

    be classified

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,22 +1,12 @@
-## used in vagrant
-exec { epel:
-  command => "rpm -Uvh http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-8.noarch.rpm",
-  creates => "/etc/yum.repos.d/epel.repo",
-  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-}
-
+## This manifest is used in vagrant provisioning
 package {
   [
     'curl',
     'unzip',
     'git',
-    'docker-io',
     ]:
   ensure => present,
-  require => Exec[epel],
 }
 
-service { docker:
-  ensure => running,
-  require => Package["docker-io"],
-}
+import "docker"
+include docker

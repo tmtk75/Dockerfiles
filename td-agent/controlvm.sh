2cbc7074beb070296600c9307284c5aa62749894
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-04-26 07:05:39 +0900

    td-agent

---
+++ td-agent/controlvm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# If you use boot2docker, you have to configure the vm for port forwrading.
+#
+VBoxManage controlvm "boot2docker-vm" natpf1 ",tcp,127.0.0.1,24224,,24224"
+VBoxManage controlvm "boot2docker-vm" natpf1 ",tcp,127.0.0.1,8888,,8888"
+VBoxManage controlvm "boot2docker-vm" natpf1 ",udp,127.0.0.1,24224,,24224"
+VBoxManage controlvm "boot2docker-vm" natpf1 ",udp,127.0.0.1,8888,,8888"
+
